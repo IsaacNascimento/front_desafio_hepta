f757a721fb588a0dbe1394610fcc1b94bea1fda0
ADIsaacNascimento <104455835+ADIsaacNascimento@users.noreply.github.com>
2022-09-12 01:06:21 -0300

    API deployment

--- src/service/constants.js
+++ src/service/constants.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = 'http://127.0.0.1:8000';
+const API = 'https://back-desafio-hepta.herokuapp.com';
 export const ApiTarefas = axios.create({  
     baseURL: API,
 })
